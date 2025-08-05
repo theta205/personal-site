174e2a5f4f2ac0f6e3107a34d54059c00aec013e
Isaiah Rohrer <isaiah.rohrer@gmail.com>
2025-08-05 16:56:44 -0400

    added favicons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,6 +8,17 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Isaiah Rohrer",
   description: "Personal website of Isaiah Rohrer, a junior Computer Science student graduating in December 2026.",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' }
+    ],
+  },
+  manifest: '/site.webmanifest'
 }
 
 export default function RootLayout({
